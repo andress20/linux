3379ab4d90c2f377d9dbbda7e19ce2fd9eb0e1f2
andres <andress20@gmail.com>
2020-08-16 01:09:39 -0500

    poniendo todo al dia

---
+++ forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from todo.models import TodoItem
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = TodoItem
+        fields = [
+            'content', 
+        ]
